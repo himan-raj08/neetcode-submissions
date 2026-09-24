e19f8a5c0ad123e04faf47759a1afe23aef6722a
himan-raj08 <himan.raj82005@gmail.com>
2026-09-24 11:04:18 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n =nums.length;
+        int[] dp =new int[n];
+        Arrays.fill(dp,-1);
+       return solve(nums,0,n,dp);  
+    }
+    public int solve(int[] nums,int i,int n,int[] dp){
+         if (i >= n) return 0;
+         if(dp[i]!=-1) return dp[i];
+         return dp[i]=Math.max((nums[i]+solve(nums,i+2,n,dp)),solve(nums,i+1,n,dp));
+    }
+}
